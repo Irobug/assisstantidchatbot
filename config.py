53254f2c61997232f8d811c2822e114d71fefb15
ɪꝛᴏ <im_maxim@inbox.ru>
2024-07-17 14:17:13 +0000

    Update config.py

--- config.py
+++ config.py
@@ -5,27 +5,27 @@ load_dotenv()
 
 
 ### ❖ ➥
-API_ID = int(getenv("API_ID", None))
+API_ID = int(getenv("API_ID", "28967047"))
 
 ### ❖ ➥
-API_HASH = getenv("API_HASH", None)
+API_HASH = getenv("API_HASH", "9d85609f45b51aa970fa13f6af3d4947")
 
 ### ❖ ➥
-STRING_SESSION = getenv("STRING_SESSION", None)
+STRING_SESSION = getenv("STRING_SESSION", "AQDNvmIASamznHxqfAXZVgnGktnFg0r7mUEoTWg1u-02T5Opmemwi4wX0ZgeRqm54MEQ9J-S_95mukcLHIDuso99N1am-J0VD3wJvJqRlRjuh71_mpBzuSD01NH1YjGT25SLMj9woa4BwHLgjNT5lagxgl_tEP_FwotmiF59yIUGHaL88e7SqyVFXj4vLyPz6aZ660FSSIJM2Gp3gHIuGU5d2wFRhZg_nwTFJUSfhwHvEbn8tMDifH9-G5P9RyJZMCGdVhNFVbuAcjCcaW3Q1ZGRCmUWLefxK6tSM_RDXFLiXZYuebajYcqMfDdZpWOEvDg4lhObxHIxMKuB0_fgRHwldST9YgAAAAF0udXmAA")
 
 ### ❖ ➥
 OWNER_ID = int(getenv("OWNER_ID", "6922271843"))
 
 ### ❖ ➥
-MONGO_URL = getenv("MONGO_URL", None)
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://kuldiprathod2003:kuldiprathod2003@cluster0.wxqpikp.mongodb.net/?retryWrites=true&w=majority")
 
 ### ❖ ➥
-SUPPORT_GRP = getenv("SUPPORT_GRP", "THE_FRIENDZ")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "Flames_xD")
 
 ### ❖ ➥
-UPDATE_CHNL = getenv("UPDATE_CHNL", "ROY_EDITX")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "xD_Feelings")
 
 ### ❖ ➥
-OWNER_USERNAME = getenv("OWNER_USERNAME", "LOCO_X_PILOT")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "NoT_uR_SoHeL")
 
 
